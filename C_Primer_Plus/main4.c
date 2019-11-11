4de1f32e6527ae62750fc293ea7e40c0043d20fc
夜雪 楼 <wujiawei2016@outlook.com>
2019-11-11 23:19:12 +0800

    新增不同数据类型对应的转换说明例题(未完成)

--- C_Primer_Plus/main4.c
+++ C_Primer_Plus/main4.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
-//第四章 字符串和格式化输入/输出 
+/*
+本章介绍以下内容：
+函数：strlen()
+关键字：const
+字符串
+如何创建、存储字符串
+如何使用strlen()函数获取字符串的长度
+用C预处理器指令#define和ANSIC的const修饰符创建符号常量
+本章重点介绍输入和输出。与程序交互和使用字符串可以编写个性化的
+程序，本章将详细介绍C语言的两个输入/输出函数：printf()和scanf()。学会
+使用这两个函数，不仅能与用户交互，还可根据个人喜好和任务要求格式化
+输出。最后，简要介绍一个重要的工具——C预处理器指令，并学习如何定
+义、使用符号常量。
+
+*/
 int main4() {
 	printf("this is main\n");
 	//example_4_1_main();
-	example_4_2_main();
+	//example_4_2_main();
+	example_4_3_main();
 	//pratice_4_1_main();
 	return 0;
 }
